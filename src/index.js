8ee1d08f88864a6e76fdf8f7f09becc0fabd9129
KONG41 <sokkongchan@gmal.com>
2023-02-28 13:57:47 +0700

    fix no image in development page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './language/i18n';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+    <React.StrictMode>
     <App />
   </React.StrictMode>
 );
